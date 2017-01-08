9eb9d26b2ef00082823ebaaeb6298f14e5f6ab0a
maviszhu <maviszhu1987@gmail.com>
2017-01-08 14:55:15 +0800

    add user to job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,6 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :check_user, only: [:edit, :update, :destroy]
   def index
     @jobs = Job.all.order('updated_at DESC')
   end
@@ -13,6 +15,7 @@ class JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
+    @job.user = current_user
     if @job.save
       redirect_to jobs_path, notice: 'Create a job!'
     else
@@ -21,11 +24,9 @@ class JobsController < ApplicationController
   end
 
   def edit
-    @job = Job.find(params[:id])
   end
 
   def update
-    @job = Job.find(params[:id])
     if @job.update(job_params)
       redirect_to jobs_path, notice: 'Update Success!'
     else
@@ -34,17 +35,21 @@ class JobsController < ApplicationController
   end
 
   def destroy
-    @job = Job.find(params[:id])
     if @job.destroy
       redirect_to jobs_path, alert: 'Job Deleted!'
     end
   end
 
 
-
-
   private
   def job_params
     params.require(:job).permit(:title, :content, :wage_min, :wage_max, :contact, :is_hidden)
+  end
+
+  def check_user
+    @job = Job.find(params[:id])
+    if current_user != @job.user
+      redirect_to jobs_path, warning: 'You have no permission!'
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170108044016) do
+ActiveRecord::Schema.define(version: 20170108063203) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170108044016) do
     t.boolean  "is_hidden"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
